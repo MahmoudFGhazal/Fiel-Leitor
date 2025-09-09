e181e5ea972686d51bd6d2a980650945b99609c6
Mahmoud Fauzi Ghazal <mahmoud.f.ghazal@gmail.com>
2025-09-09 00:54:22 -0300

    feat: apiRouter - Front

---
+++ fiel-front/src/api/objects.ts
@@ -0,0 +1,88 @@
+export interface Address {
+    id: number,
+    user: User,
+    nickname: string,
+    number: string,
+    complement: string,
+    street: string,
+    neighborhood: string,
+    zip: string,
+    city: string,
+    state: string,
+    country: string,
+    streetType: StreetType,
+    residenceType: ResidenceType
+}
+
+export interface Card {
+    id: number,
+    user: User,
+    principal: boolean,
+    number: string,
+    ccv: string,
+    holder: string,
+    valid: string,
+    paymentType: PaymentType
+}
+
+export interface Gender {
+    id: number,
+    gender: string
+}
+
+export interface PaymentType {
+    id: number,
+    paymentType: string
+}
+
+export interface ResidenceType {
+    id: number,
+    residenceType: string
+}
+
+export interface StreetType {
+    id: number,
+    streetType: string
+}
+
+export interface User {
+    id: number,
+    email: string,
+    password: string,
+    name: string,
+    active: boolean,
+    gender: Gender,
+    birthday: Date,
+    cpf: string,
+    phoneNumber: string
+}
+
+export interface ApiResponse {
+    data: {
+        entity: Datas,
+        entities: Datas,
+        page: number,
+        limit: number,
+        pageCount: number,
+        totalItem: number,
+        totalPage: number
+    },
+    typeResponse: string,
+    message: string
+}
+
+export type Datas =
+  | Address
+  | Card
+  | Gender
+  | PaymentType
+  | ResidenceType
+  | StreetType
+  | User
+  | Address[]
+  | Card[]
+  | Gender[]
+  | PaymentType[]
+  | ResidenceType[]
+  | StreetType[]
+  | User[];
