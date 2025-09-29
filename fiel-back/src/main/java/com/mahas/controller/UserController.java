37bb90088dbb5a7a20ff0ff92977cfc3e46d72f7
Mahmoud Fauzi Ghazal <mahmoud.f.ghazal@gmail.com>
2025-09-29 09:11:47 -0300

    fix: deleteUserReturn = back

--- fiel-back/src/main/java/com/mahas/controller/UserController.java
+++ fiel-back/src/main/java/com/mahas/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.mahas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mahas.command.post.rules.DeleteUserAdapter;
 import com.mahas.command.post.rules.LoginAdapter;
 import com.mahas.command.post.rules.SetActiveUserAdapter;
 import com.mahas.command.post.rules.SignAddressAdapter;
@@ -20,19 +34,6 @@ import com.mahas.dto.request.user.UserDTORequest;
 import com.mahas.dto.response.DTOResponse;
 import com.mahas.dto.response.user.UserDTOResponse;
 import com.mahas.facade.IFacade;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @CrossOrigin(origins = "*")
@@ -78,6 +79,9 @@ public class UserController {
 
     @Autowired
     private SetActiveUserAdapter setActiveUserAdapter;
+
+    @Autowired
+    private DeleteUserAdapter deleteUserAdapter;
 
     @PostMapping("/login")
     public ResponseEntity<FacadeResponse> Login(@RequestBody UserDTORequest user) {
@@ -192,6 +196,7 @@ public class UserController {
         user.setId(id);
         request.setEntity(user);
         request.setPreCommand(verifyDeleteUser);
+        request.setPostCommand(deleteUserAdapter);
         
         FacadeResponse response = facade.delete(request);
         
