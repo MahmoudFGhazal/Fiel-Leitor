08e3443cdcef86e08c2da59dde12bbe2e664235d
Mahmoud Fauzi Ghazal <mahmoud.f.ghazal@gmail.com>
2025-10-23 23:20:22 -0300

    fix: getSales - front

--- fiel-front/src/components/order/orderItem/index.tsx
+++ fiel-front/src/components/order/orderItem/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { SaleBookResponse } from '@/api/dtos/responseDTOs';
+import Button from '@/components/buttonComponents/button';
 import Image from 'next/image';
 import bookImage from '../../../../public/book.png';
 import styles from './orderItem.module.css';
@@ -7,17 +8,32 @@ import styles from './orderItem.module.css';
 export default function OrderItem({ book }: { book: SaleBookResponse }) {
     return (
         <div className={styles.orderItem}>
-            <Image
-                src={bookImage}
-                alt={book.book?.name ?? ""}
-                className={styles.fieldImage}
-            />
+            <div className={styles.fieldContent} >
+                <Image
+                    src={bookImage}
+                    alt={book.book?.name ?? ""}
+                    className={styles.fieldImage}
+                />
+            </div>
             <div className={styles.itemInfo}>
-                <p>{book.book?.name}</p>
-                <p>Quantidade: {book.quantity}</p>
-                <div className={styles.itemButtons}>
-                    <button>Comprar novamente</button>
-                    <button>Ver o seu item</button>
+                <div className={styles.infoContent}>
+                    <p>{book.book?.name}</p>
+                    <p>Valor: R${book.price}</p>
+                    <p>Quantidade: {book.quantity}</p>
+                </div>
+                <div className={styles.buttonContainer}>
+                    <div className={styles.itemButtons}>
+                        <Button 
+                            type={'button'}
+                        >
+                            Adicionar no Carrinho
+                        </Button>
+                        <Button 
+                            type={'button'}
+                        >
+                            Ver Item
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
